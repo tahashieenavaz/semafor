e20d92fd79ab62767111904ae0e486550aa6b81b
tahashieenavaz <tahashieenavaz@gmail.com>
2025-09-26 15:39:59 +0200

    nah

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="https://cdn.jsdelivr.net/npm/marked/lib/marked.umd.js"></script>
+
+    <title>Semafor</title>
+
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+
+</html>
---
+++ resources/views/components/toolbar.blade.php
@@ -0,0 +1,10 @@
+@props([
+    'items' => ['bold', 'code', 'duplicate', 'heading'],
+])
+<div data-toolbar class="hidden absolute rounded-xl p-2 bg-black shadow text-white flex gap-2">
+    @foreach ($items as $item)
+        <button class="hover:bg-white/50 transition rounded p-0.5 cursor-pointer">
+            <img src="{{ asset('/images/' . $item . '.svg') }}" class="size-6 invert" />
+        </button>
+    @endforeach
+</div>
