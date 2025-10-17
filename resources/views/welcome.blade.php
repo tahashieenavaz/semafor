f72fbed98e3c5ab75fd959ae031ba6aa3b07187f
tahashieenavaz <tahashieenavaz@gmail.com>
2025-10-17 14:40:44 +0200

    feat: adds a two column layout to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
 <x-layout>
-
+    <div class="grid grid-cols-[1fr_200px] max-w-250 mx-auto">
+        <main>
+            main
+        </main>
+        <aside>
+            sdfsdfsf
+        </aside>
+    </div>
 </x-layout>
