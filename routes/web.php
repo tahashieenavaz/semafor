9c70f2ca704e280a151bd73c9c9e9a17cd8f21ed
tahashieenavaz <tahashieenavaz@gmail.com>
2025-10-17 14:43:35 +0200

    feat: adds show note endpoint to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\Note;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get("/note/{note}", function (Note $note) {
+    return $note;
 });
