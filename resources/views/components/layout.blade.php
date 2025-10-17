2de547c234138dfec9163acac42fac0379d6f1dc
tahashieenavaz <tahashieenavaz@gmail.com>
2025-10-17 14:38:49 +0200

    feat: resets the project

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -6,10 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <script src="https://cdn.jsdelivr.net/npm/marked/lib/marked.umd.js"></script>
-
     <title>Semafor</title>
-
 </head>
 
 <body>
