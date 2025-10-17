8a0010aafef8c97dcac8b43417ab3e1f4e9442f0
tahashieenavaz <tahashieenavaz@gmail.com>
2025-10-17 14:52:18 +0200

    ui: removes two column layout from welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,7 @@
 <x-layout>
-    <div class="grid grid-cols-[1fr_200px] max-w-250 mx-auto">
+    <div class="grid max-w-250 mx-auto">
         <main>
             main
         </main>
-
-        <aside>
-            @forelse($notes as $note)
-                <a href="{{ $note->url() }}">
-                    {{ $note->title }}
-                </a>
-            @empty
-                No notes yet.
-            @endforelse
-        </aside>
     </div>
 </x-layout>
